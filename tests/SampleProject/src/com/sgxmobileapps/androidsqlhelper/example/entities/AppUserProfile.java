b07df35d4f218f66600dc0ba5758c315a6646638
Massimo Gaddini <massimo.gaddini@gmail.com>
2011-12-30 15:20:24 +0100

    Sample project package refatoring

--- tests/SampleProject/src/com/sgxmobileapps/androidsqlhelper/example/entities/AppUserProfile.java
+++ tests/SampleProject/src/com/sgxmobileapps/androidsqlhelper/example/entities/AppUserProfile.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.  
  */
-package com.sgxmobileapps.androidsqlhelper.test.entities;
+package com.sgxmobileapps.androidsqlhelper.example.entities;
 
 import com.sgxmobileapps.androidsqlhelper.annotation.PersistentEntity;
 import com.sgxmobileapps.androidsqlhelper.annotation.PersistentField;
